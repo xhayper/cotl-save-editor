9c4553004fa746145e0a2e6cd742b0b0e97eff40
xhayper <hayper1919@gmail.com>
2022-09-04 16:13:14 +0700

    feat: stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,20 @@
 import { useSaveStore } from "@/stores/save";
 
 const save = useSaveStore();
+
+const loadFile = (payload: Event<EventTarget | HTMLInputElement>) => {
+    if (!(payload.target instanceof HTMLInputElement)) return;
+    const file = payload.target.files![0];
+
+    const reader = new FileReader();
+    reader.onloadend = (ev: ProgressEvent<FileReader>) => {
+        save.load({ fileName: file.name, data: new Uint8Array(ev.target!.result! as ArrayBuffer) });
+    };
+    reader.readAsArrayBuffer(file);
+};
 </script>
 
 <template>
-    {{ save.cultName }}
+    <input type="file" id="file-selector" accept=".json" v-on:change="loadFile" />
+    {{ save.saveData.CultFaith }}
 </template>
