0d1eb5bcb3623cd0c9847071c127dbe1d0977acf
xhayper <46065349+xhayper@users.noreply.github.com>
2022-09-04 16:13:14 +0700

    feat: stuff

--- src/stores/save.ts
+++ src/stores/save.ts
@@ -1,14 +1,40 @@
+import type { SaveData } from "./types";
 import { defineStore } from "pinia";
 
 export const useSaveStore = defineStore({
     id: "save",
-    state: () => ({
-        cultName: "Cult of the Lamb"
-    }),
+    state: () => ({ saveData: {} as SaveData, fileName: "slot_0.json" }),
     getters: {},
     actions: {
-        setCultName(name: string) {
-            this.cultName = name;
+        async load(ctx: { data: Uint8Array; fileName: string }) {
+            const { data, fileName } = ctx;
+
+            this.fileName = fileName;
+            this.saveData = {} as any;
+
+            if (data.at(0) === 69) {
+                const key = await window.crypto.subtle.importKey(
+                    "raw",
+                    data.subarray(1, 17),
+                    {
+                        name: "AES-CBC",
+                        length: 128
+                    },
+                    false,
+                    ["decrypt"]
+                );
+                const decrypted = await window.crypto.subtle.decrypt(
+                    {
+                        name: "AES-CBC",
+                        iv: data.subarray(17, 33)
+                    },
+                    key,
+                    data.subarray(33)
+                );
+                Object.assign(this.saveData, JSON.parse(new TextDecoder().decode(decrypted)));
+            } else {
+                Object.assign(this.saveData, JSON.parse(new TextDecoder().decode(data)));
+            }
         }
     }
 });
