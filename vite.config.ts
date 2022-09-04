24129ea2ecb35861ebc430baa450c7fcc82e2010
xhayper <hayper1919@gmail.com>
2022-09-04 14:05:22 +0700

    feat: some initial page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+            "~bootstrap-icons": path.resolve(__dirname, "node_modules/bootstrap-icons")
+        }
+    },
     build: {
         sourcemap: true
     }
