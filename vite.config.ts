bba870b529f1b1e87fd70b3ae68cecad008276dc
xhayper <46065349+xhayper@users.noreply.github.com>
2022-09-06 09:20:08 +0700

    feat: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,48 @@
 import { fileURLToPath, URL } from "node:url";
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
+    plugins: [
+        vue(),
+        vueJsx(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                short_name: "Save editor",
+                name: "Cult of the Lamb save editor",
+                description: "Save file editor for Cult of the Lamb",
+                icons: [
+                    {
+                        src: "/icon.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    },
+                    {
+                        src: "/mask-icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ],
+                start_url: "/",
+                background_color: "#343A40",
+                display: "fullscreen",
+                scope: "/",
+                theme_color: "#CD2524"
+            },
+            workbox: {
+                additionalManifestEntries: [{ url: "/", revision: null }]
+            }
+        })
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -16,6 +52,6 @@ export default defineConfig({
         }
     },
     build: {
-        sourcemap: "inline"
+        sourcemap: true
     }
 });
