e3b0070c8503daabb4ce932c911c90ce51e3fb19
xhayper <46065349+xhayper@users.noreply.github.com>
2022-10-13 12:03:42 +0700

    feat: forgot some file

---
+++ components/GTag.vue
@@ -0,0 +1,24 @@
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+
+onMounted(() => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", runtimeConfig.public.GTAG_ID);
+});
+</script>
+
+<template>
+  <client-only>
+    <!-- Google tag (gtag.js) -->
+    <Head>
+      <Script
+        type="text/partytown"
+        :src="`https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.public.GTAG_ID}`"
+      />
+    </Head>
+  </client-only>
+</template>
---
+++ components/ThemeLoader.vue
@@ -0,0 +1,27 @@
+<script setup>
+// Prevent FOUC
+useHead({
+  script: [
+    {
+      children: `
+(() => {
+    const root = document.documentElement;
+    const theme = localStorage.getItem("theme");
+    if (
+        theme === "dark" ||
+        (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        root.classList.add("dark");
+    } else {
+        root.classList.remove("dark");
+    }
+})();
+`,
+    },
+  ],
+});
+
+useDark();
+</script>
+
+<template><div /></template>
