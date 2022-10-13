461ce8a5a65e89729b9bfa2007526132e9cf76a6
xhayper <46065349+xhayper@users.noreply.github.com>
2022-10-13 12:18:47 +0700

    feat: try stuff

--- components/GTag.vue
+++ components/GTag.vue
@@ -1,24 +1,26 @@
 <script setup>
 const runtimeConfig = useRuntimeConfig();
 
-onMounted(() => {
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag("js", new Date());
-  gtag("config", runtimeConfig.public.GTAG_ID);
+useHead({
+  script: [
+    {
+      type: "text/partytown",
+      async: true,
+      src: `https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.public.GTAG_ID}`,
+    },
+    {
+      type: "text/partytown",
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${runtimeConfig.public.GTAG_ID}');
+      `,
+    },
+  ],
 });
 </script>
 
 <template>
-  <client-only>
-    <!-- Google tag (gtag.js) -->
-    <Head>
-      <Script
-        type="text/partytown"
-        :src="`https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.public.GTAG_ID}`"
-      />
-    </Head>
-  </client-only>
+  <div />
 </template>
